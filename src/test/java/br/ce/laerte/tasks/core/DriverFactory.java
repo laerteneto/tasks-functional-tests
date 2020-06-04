9fbac70a5dab9c380a0a579173d0e34eaec0cfa7
Laerte Mello <laerte-m@hotmail.com>
2020-06-04 14:51:34 -0300

    setting headless mode

--- src/test/java/br/ce/laerte/tasks/core/DriverFactory.java
+++ src/test/java/br/ce/laerte/tasks/core/DriverFactory.java
@@ -4,6 +4,7 @@ import java.util.concurrent.TimeUnit;
 
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
 
 public class DriverFactory {
 	private static WebDriver driver;
@@ -23,9 +24,12 @@ public class DriverFactory {
 	}
 
 	public static void createDriver() {
+		ChromeOptions options = new ChromeOptions();
+		options.setHeadless(true);
+
 		System.setProperty("webdriver.chrome.driver",
 				System.getProperty("user.dir") + "/src/test/resources/drivers/chromedriver");
-		driver = new ChromeDriver();
+		driver = new ChromeDriver(options);
 		driver.navigate().to("http://localhost:8001/tasks/");
 		driver.manage().window().maximize();
 		driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
