7212077fa85ff9f60e7745057650e490c73fea58
Laerte Mello <laerte-m@hotmail.com>
2020-06-04 17:59:58 -0300

    adding remote driver

--- src/test/java/br/ce/laerte/tasks/core/DriverFactory.java
+++ src/test/java/br/ce/laerte/tasks/core/DriverFactory.java
@@ -1,17 +1,28 @@
 package br.ce.laerte.tasks.core;
 
+import java.net.MalformedURLException;
+import java.net.URL;
 import java.util.concurrent.TimeUnit;
 
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.chrome.ChromeOptions;
+import org.openqa.selenium.remote.DesiredCapabilities;
+import org.openqa.selenium.remote.RemoteWebDriver;
 
 public class DriverFactory {
 	private static WebDriver driver;
 
 	public static WebDriver getDriver() {
-		if (driver == null)
-			createDriver();
+		if (driver == null) {
+			// createDriver();
+			try {
+				createRemoteDriver();
+			} catch (MalformedURLException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
+		}
 
 		return driver;
 	}
@@ -34,4 +45,19 @@ public class DriverFactory {
 		driver.manage().window().maximize();
 		driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
 	}
+
+	public static void createRemoteDriver() throws MalformedURLException {
+		System.setProperty("webdriver.chrome.driver",
+				System.getProperty("user.dir") + "/src/test/resources/drivers/chromedriver");
+		DesiredCapabilities cap = DesiredCapabilities.chrome();
+		try {
+			driver = new RemoteWebDriver(new URL("http://172.17.0.1:4444/wd/hub"), cap);
+		} catch (MalformedURLException e) {
+			e.printStackTrace();
+		}
+		driver.navigate().to("http://localhost:8001/tasks/");
+		driver.quit();
+
+	}
+
 }
