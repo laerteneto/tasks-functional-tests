ca7addfe1dfd78e81f819d4079ffed116cb19683
Laerte Mello <laerte-m@hotmail.com>
2020-06-07 16:50:29 -0300

    ajusting drivers in threads to let them work in parallel

--- src/test/java/br/ce/laerte/tasks/core/DriverFactory.java
+++ src/test/java/br/ce/laerte/tasks/core/DriverFactory.java
@@ -5,54 +5,45 @@ import java.net.URL;
 import java.util.concurrent.TimeUnit;
 
 import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.chrome.ChromeOptions;
 import org.openqa.selenium.remote.RemoteWebDriver;
 
 public class DriverFactory {
-	private static WebDriver driver;
-
-	public static WebDriver getDriver() {
-		if (driver == null) {
-			// createDriver();
-			createRemoteDriver();
-
+	private static ThreadLocal<WebDriver> threadDriver = new ThreadLocal<WebDriver>() {
+		@Override
+		protected synchronized WebDriver initialValue() {
+			return initDriver();
 		}
-		return driver;
-	}
+	};
 
-	public static void killDriver() {
-		if (driver != null) {
-			driver.quit();
-			driver = null;
-		}
-	}
-
-	public static void createDriver() {
-		ChromeOptions options = new ChromeOptions();
-		options.setHeadless(true);
-
-		System.setProperty("webdriver.chrome.driver",
-				System.getProperty("user.dir") + "/src/test/resources/drivers/chromedriver");
-		driver = new ChromeDriver(options);
-		driver.navigate().to("http://localhost:8001/tasks/");
-		driver.manage().window().maximize();
-		driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
-	}
-
-	public static void createRemoteDriver() {
-		// System.setProperty("webdriver.chrome.driver", System.getProperty("user.dir") + "/src/test/resources/drivers/chromedriver");
-		// DesiredCapabilities cap = DesiredCapabilities.chrome(); // deprecated
+	// Used to remote executions in parallel
+	public static WebDriver initDriver() {
+		WebDriver driver = null;
 		ChromeOptions cap = new ChromeOptions();
-		cap.setHeadless(true);
+		// cap.setHeadless(true);
 		try {
 			driver = new RemoteWebDriver(new URL("http://172.17.0.1:4444/wd/hub"), cap);
 		} catch (MalformedURLException e) {
 			e.printStackTrace();
 		}
-		driver.navigate().to("http://192.168.0.178:8001/tasks/");
-		// driver.manage().window().maximize();
 		driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
+
+		return driver;
+	}
+
+	public static WebDriver getDriver() {
+		return threadDriver.get();
+	}
+
+	public static void killDriver() {
+		WebDriver driver = getDriver();
+		if (driver != null) {
+			driver.quit();
+			driver = null;
+		}
+		if (threadDriver != null) {
+			threadDriver.remove();
+		}
 	}
 
 }
--- src/test/java/br/ce/laerte/tasks/functional/BaseTest.java
+++ src/test/java/br/ce/laerte/tasks/functional/BaseTest.java
@@ -1,7 +1,7 @@
 package br.ce.laerte.tasks.functional;
 
 import org.junit.After;
-import org.junit.AfterClass;
+import org.junit.Before;
 import org.junit.Rule;
 import org.junit.rules.TestName;
 
@@ -12,9 +12,10 @@ public class BaseTest {
 	@Rule
 	public TestName testName = new TestName(); // obter parametros do test para utilizar no screenshot depois
 
-	@AfterClass
-	public static void finalizarClasse() {
-		DriverFactory.killDriver();
+	@Before
+	public void setup() {
+		DriverFactory.getDriver().navigate().to("http://192.168.0.178:8001/tasks/");
+		// driver.manage().window().maximize();
 	}
 
 	@After
